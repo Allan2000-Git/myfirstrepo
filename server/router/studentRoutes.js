bab96bb436a1f677ed2fc52a181320cfb9dee350
Allan Joston Fernandes <allanfernandes459@gmail.com>
2023-09-23 11:51:02 +0530

    server commit

--- server/router/studentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerStudent, loginStudent } = require('../controllers/studentControllers');
-
-router.route('/register').post(registerStudent);
-router.post("/login", loginStudent);
-
-module.exports = router;
